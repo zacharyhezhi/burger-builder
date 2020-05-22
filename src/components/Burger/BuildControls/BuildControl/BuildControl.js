647966c9033038b7b20e0e611a266e8d42d71074
Zachary <hezhibruno@gmail.com>
2020-05-22 17:30:43 +1000

    Multiple controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,7 +6,6 @@ const buildControl = (props) => (
         <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>Less</button>
         <button className={classes.More}>More</button>
-
     </div>
 );
 
